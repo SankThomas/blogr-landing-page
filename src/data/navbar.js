0423a972b0c39b8355074d351cbd71522a22d668
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 01:01:05 +0300

    navbar data array

---
+++ src/data/navbar.js
@@ -0,0 +1,34 @@
+export const navbar = [
+  {
+    id: 1,
+    title: "Products",
+    dropdown: [
+      "Overview",
+      "Pricing",
+      "Marketplace",
+      "Features",
+      "Integrations",
+    ],
+  },
+  {
+    id: 2,
+    title: "Company",
+    dropdown: ["About", "Team", "Blog", "Careers"],
+  },
+  {
+    id: 3,
+    title: "Connect",
+    dropdown: ["Contact", "Newsletter", "Linkedin"],
+  },
+]
+
+export const buttons = [
+  {
+    id: 4,
+    title: "Login",
+  },
+  {
+    id: 5,
+    title: "Sign Up",
+  },
+]
