2d5eb761104e772592326582af4d02598207acd2
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 01:00:57 +0300

    footer data array

---
+++ src/data/footer.js
@@ -0,0 +1,17 @@
+export const footer = [
+  {
+    id: 1,
+    title: "Product",
+    links: ["Overview", "Pricing", "Marketplace", "Features", "Integrations"],
+  },
+  {
+    id: 2,
+    title: "Company",
+    links: ["About", "Team", "Blog", "Careers"],
+  },
+  {
+    id: 3,
+    title: "Connect",
+    links: ["Contact", "Newsletter", "Linkedin"],
+  },
+]
