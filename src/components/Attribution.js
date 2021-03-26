af209622f7658fba99d2900974c4ebe035429177
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 00:57:19 +0300

    Attribution text component

---
+++ src/components/Attribution.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+const Attribution = () => {
+  return (
+    <>
+      <div className="attribution text-white">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopenner noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://tsbsankara.co.ke"
+          target="_blank"
+          rel="noopenner noreferrer"
+        >
+          Thomas Sankara
+        </a>
+        .
+      </div>
+    </>
+  )
+}
+
+export default Attribution
