bbaa1d01bf951879867194f9292438ab64cde653
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 01:00:04 +0300

    Free, Open, Simple

---
+++ src/components/Free.js
@@ -0,0 +1,42 @@
+import React from "react"
+import freeMobileImage from "../images/illustration-laptop-mobile.svg"
+import freeDesktopImage from "../images/illustration-laptop-desktop.svg"
+
+const Free = () => {
+  return (
+    <>
+      <section className="px-5 pb-20">
+        <div>
+          <picture>
+            <source media="(min-width: 768px)" srcSet={freeDesktopImage} />
+            <img src={freeMobileImage} alt="laptop" />
+          </picture>
+        </div>
+
+        <section>
+          <div className="mt-10 text-center">
+            <h3>Free, open, simple</h3>
+            <p className="leading-8 text-gray-600">
+              Blogr is a free and open source application backed by a large
+              community of helpful developers. It supports features such as code
+              syntax highlighting, RSS feeds, social media integration,
+              third-party commenting tools, and works seamlessly with Google
+              Analytics. The architecture is clean as is relatively easy to
+              learn.
+            </p>
+          </div>
+          <div className="mt-10 text-center">
+            <h3>Powerful tooling</h3>
+            <p className="leading-8 text-gray-600">
+              Batteries included. We built a simple and straightforward CLI
+              tools that makes customization and deployment a breeze, but
+              capable of producing even the most complicated sites.
+            </p>
+          </div>
+        </section>
+      </section>
+    </>
+  )
+}
+
+export default Free
