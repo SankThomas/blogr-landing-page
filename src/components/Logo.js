e6bcf3e809390bcb9bb4213bdee4f87a39399e6f
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 01:00:19 +0300

    Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "../images/logo.svg"
+
+const Logo = () => {
+  return (
+    <>
+      <div>
+        <img src={logo} alt="Blogr" className="block mx-auto" />
+      </div>
+    </>
+  )
+}
+
+export default Logo
