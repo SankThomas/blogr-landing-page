4b638764e6fb61da5662ff9ec2c09b770eccaf6f
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 14:57:43 +0300

    desktop responsive

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -8,10 +8,10 @@ const Showcase = () => {
     <>
       <section className="showcase">
         <div className="overlay py-44 px-5 text-center">
-          <h1 className="font-bold text-4xl text-white mb-2">
+          <h1 className="font-bold text-4xl text-white mb-2 md:text-5xl lg:text-6xl xl:text-7xl">
             A modern publishing platform
           </h1>
-          <p className="text-white mb-5">
+          <p className="text-white mb-5 lg:text-lg">
             Grow your audience and build your online brand
           </p>
           <Buttons />
