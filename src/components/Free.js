eaf14b7dfa184fc4a31f07a94bb61aa4a31d97cb
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 14:57:30 +0300

    desktop responsive

--- src/components/Free.js
+++ src/components/Free.js
@@ -5,7 +5,7 @@ import freeDesktopImage from "../images/illustration-laptop-desktop.svg"
 const Free = () => {
   return (
     <>
-      <section className="px-5 pb-20">
+      <section className="px-5 pb-20 md:grid md:grid-cols-2 xl:max-w-7xl xl:mx-auto">
         <div>
           <picture>
             <source media="(min-width: 768px)" srcSet={freeDesktopImage} />
@@ -15,8 +15,8 @@ const Free = () => {
 
         <section>
           <div className="mt-10 text-center">
-            <h3>Free, open, simple</h3>
-            <p className="leading-8 text-gray-600">
+            <h3 className="md:text-left">Free, open, simple</h3>
+            <p className="leading-8 text-gray-600 md:text-left">
               Blogr is a free and open source application backed by a large
               community of helpful developers. It supports features such as code
               syntax highlighting, RSS feeds, social media integration,
@@ -26,8 +26,8 @@ const Free = () => {
             </p>
           </div>
           <div className="mt-10 text-center">
-            <h3>Powerful tooling</h3>
-            <p className="leading-8 text-gray-600">
+            <h3 className="md:text-left">Powerful tooling</h3>
+            <p className="leading-8 text-gray-600 md:text-left">
               Batteries included. We built a simple and straightforward CLI
               tools that makes customization and deployment a breeze, but
               capable of producing even the most complicated sites.
