59faaeae1a8dfc09d2cd07412e6626c5063f440c
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 14:57:01 +0300

    update Buttons component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,9 +2,9 @@ import React from "react"
 
 const Buttons = () => {
   return (
-    <div className="flex items-center justify-evenly">
+    <div className="flex items-center justify-evenly md:justify-center">
       <div>
-        <button className="bg-white border-2 border-white pt-3 pb-2 px-4 rounded-full text-red-500 font-bold">
+        <button className="bg-white border-2 border-white pt-3 pb-2 px-4 rounded-full text-red-500 font-bold md:mr-4">
           Start for Free
         </button>
       </div>
