7944c006b98251528b9bb205bf120866663f3cc5
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 14:57:34 +0300

    desktop responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,30 @@ import close from "../images/icon-close.svg"
 
 const Header = () => {
   const [open, setOpen] = React.useState(false)
+  // eslint-disable-next-line
+  const [width, setWidth] = React.useState(768)
+
+  React.useEffect(() => {
+    if (window.innerWidth >= width) {
+      setOpen(true)
+    }
+  }, [width])
 
   return (
-    <>
-      <header className="flex items-center justify-between p-5 absolute w-full z-50">
+    <div className="relative">
+      <header className="flex items-center justify-between p-5 absolute w-full z-50 xl:px-44">
         <div>
           <Logo />
         </div>
 
         {open && (
-          <nav>
+          <nav className="absolute top-20 bg-white w-11/12 p-5 text-center shadow-2xl md:bg-transparent md:shadow-none md:p-0 md:top-0 md:relative md:w-auto md:flex">
             <Navbar />
             <CallToAction />
           </nav>
         )}
 
-        <div>
+        <div className="md:hidden">
           {open ? (
             <button onClick={() => setOpen(false)} className="cursor-pointer">
               <img src={close} alt="close" />
@@ -34,7 +42,7 @@ const Header = () => {
           )}
         </div>
       </header>
-    </>
+    </div>
   )
 }
 
