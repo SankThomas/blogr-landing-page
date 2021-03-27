2caf5690e08ec2b17dd4be34f5cc44104e4625be
Thomas Sankara <thomassankara74@gmail.com>
2021-03-27 14:57:11 +0300

    update component

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -6,10 +6,13 @@ const CallToAction = () => {
   const [items, setItems] = React.useState(buttons)
   return (
     <>
-      <div>
+      <div className="flex items-center justify-center flex-col md:flex-row">
         {items.map((item) => {
           return (
-            <button key={item.id} className="block">
+            <button
+              key={item.id}
+              className="block my-2 md:my-0 md:mx-2 md:text-white"
+            >
               {item.title}
             </button>
           )
